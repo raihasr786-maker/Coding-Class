0d8dbab4d4ea825b27b3dfdce653f8681cd37921
raihasr786-maker <raihasr786@gmail.com>
2025-11-22 11:16:19 -0600

    Added Fuctions

--- Adding 1-100 loop.py
+++ Adding 1-100 loop.py
@@ -1,4 +1,13 @@
-sum=0
-for i in range(1,11):
-    sum=sum+i
-print(f"The sum of numbers frokm 1 to 10 is {sum}")
+def adding_value(starting,ending):    
+    sum=0
+    for i in range(starting,ending+1):
+        sum=sum+i
+    return sum
+def main():
+    start=int(input("Type in the starting value-> "))
+    end=int(input("Type in the ending value-> "))
+    result_func=adding_value(start,end)
+    print(f"The sum from {start} to {end} is {result_func}")
+
+if __name__ == "__main__":
+    main()
